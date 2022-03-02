380932a14113ac37536498f0dbae394aa334f415
choc0099 <40311640+choc0099@users.noreply.github.com>
2022-03-02 11:51:26 +1100

    Login Page created
    Login page prototype v1.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,9 +2,21 @@ import React from "react";
 
 function Login (){
     return (
-        <h1>
-            Login Page
-        </h1>
+        <div>
+            <h1>
+                Login Page
+            </h1>
+            <form>
+                <label className="App-form">Username:</label>
+                <input className="App-form" id="username"/>
+                <br/>
+                <label className="App-form">Password:</label>
+                <input type="password" className="App-form" id="password"/>
+                <br/>
+                <input className="App-form-button" type="submit" value="Login"/>
+            </form>
+        </div>
+
     )
 }
 
